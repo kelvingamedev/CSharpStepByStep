54f4ec385a9bc0bd4274f5338bf72a38d43db339
Kelvin <kelvin.gamedeveloper@gmail.com>
2016-01-03 15:54:44 -0200

    First program developed

---
+++ TextHello/TextHello/Program.cs
@@ -0,0 +1,14 @@
+//using System;
+
+
+namespace TextHello
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Hello World");
+
+        }
+    }
+}
