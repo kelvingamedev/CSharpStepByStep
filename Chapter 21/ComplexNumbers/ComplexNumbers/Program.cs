1b70150bd59b99cd11241ed3814775eba0d144fa
Kelvin <kelvin.gamedeveloper@gmail.com>
2016-01-17 15:52:57 -0200

    Update até o começo do capítulo 21

--- Chapter 21/ComplexNumbers/ComplexNumbers/Program.cs
+++ Chapter 21/ComplexNumbers/ComplexNumbers/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace ComplexNumbers
 {
@@ -6,7 +6,58 @@ namespace ComplexNumbers
     {
         static void DoWork()
         {
-            // to do
+            Complex first = new Complex(10, 4);
+            Complex second = new Complex(5, 2);
+
+            Console.WriteLine("first is {0}", first);
+            Console.WriteLine("second is {0}", second);
+
+            Complex temp = first + second;
+            Console.WriteLine("Add: result is {0}", temp);
+
+            temp = first - second;
+            Console.WriteLine("Subtract: result is {0}", temp);
+
+            temp = first * second;
+            Console.WriteLine("Multiply: result is {0}", temp);
+
+            temp = first / second;
+            Console.WriteLine("Divide: result is {0}", temp);
+
+            if(temp == first)
+            {
+                Console.WriteLine("Comparisson: temp == first");
+            }
+            else
+            {
+                Console.WriteLine("Comparisson: temp != first");
+            }
+
+            if(temp == temp)
+            {
+                Console.WriteLine("Comparisson: temp == temp");
+            }
+            else
+            {
+                Console.WriteLine("Comparisson: temp != temp");
+            }
+
+            Console.WriteLine("Current value of temp is {0}", temp);
+
+            if(temp == 2)
+            {
+                Console.WriteLine("Comparison after conversion: temp == 2");
+            }
+            else
+            {
+                Console.WriteLine("Comparison after conversion: temp != 2");
+            }
+
+            temp += 2;
+            Console.WriteLine("Value after adding 2: temp = {0}", temp);
+
+            int tempInt = (int) temp;
+            Console.WriteLine("Int value after conversion: tempInt = {0}", tempInt);
         }
 
         static void Main()
